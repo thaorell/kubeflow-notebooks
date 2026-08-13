adrielparedes/workbenches

refactor(app): remove duplicated constants and fix FD leak
- Remove inline const block from app.go (duplicates api/constants/)
- Move UserPath to constants/paths.go
- Reference constants.UserPath and constants.PathPrefix
- Fix file descriptor leak in static file existence check

workspaces/backend/api/app.go
@@ -35,45 +35,6 @@ import (
 	_ "github.com/kubeflow/notebooks/workspaces/backend/openapi"
 )
 
-const (
-	Version    = "1.0.0"
-	PathPrefix = "/api/v1"
-
-	MediaTypeJson = "application/json"
-	MediaTypeYaml = "application/yaml"
-
-	NamespacePathParam    = "namespace"
-	ResourceNamePathParam = "name"
-
-	// healthcheck
-	HealthCheckPath = PathPrefix + "/healthcheck"
-
-	// user
-	UserPath = PathPrefix + "/user"
-
-	// workspaces
-	AllWorkspacesPath         = PathPrefix + "/workspaces"
-	WorkspacesByNamespacePath = AllWorkspacesPath + "/:" + NamespacePathParam
-	WorkspacesByNamePath      = AllWorkspacesPath + "/:" + NamespacePathParam + "/:" + ResourceNamePathParam
-	WorkspaceActionsPath      = WorkspacesByNamePath + "/actions"
-	PauseWorkspacePath        = WorkspaceActionsPath + "/pause"
-
-	// workspacekinds
-	AllWorkspaceKindsPath    = PathPrefix + "/workspacekinds"
-	WorkspaceKindsByNamePath = AllWorkspaceKindsPath + "/:" + ResourceNamePathParam
-
-	// namespaces
-	AllNamespacesPath = PathPrefix + "/namespaces"
-
-	// secrets
-	SecretsByNamespacePath = PathPrefix + "/secrets/:" + NamespacePathParam
-	SecretsByNamePath      = SecretsByNamespacePath + "/:" + ResourceNamePathParam
-
-	// swagger
-	SwaggerPath    = PathPrefix + "/swagger/*any"
-	SwaggerDocPath = PathPrefix + "/swagger/doc.json"
-)
-
 type App struct {
 	Config               *config.EnvConfig
 	logger               *slog.Logger
@@ -119,7 +80,7 @@ func (a *App) Routes() http.Handler {
 	router.GET(constants.HealthCheckPath, a.GetHealthcheckHandler)
 
 	// user
-	router.GET(UserPath, a.GetUserHandler)
+	router.GET(constants.UserPath, a.GetUserHandler)
 
 	// namespaces
 	router.GET(constants.AllNamespacesPath, a.GetNamespacesHandler)
@@ -167,7 +128,7 @@ func (a *App) Routes() http.Handler {
 	mux := http.NewServeMux()
 
 	// API routes - handle /api/v1/* paths
-	mux.Handle(PathPrefix+"/", a.recoverPanic(a.enableCORS(router)))
+	mux.Handle(constants.PathPrefix+"/", a.recoverPanic(a.enableCORS(router)))
 
 	// Static file server for frontend assets (Module Federation support)
 	if a.Config.StaticAssetsDir != "" {
@@ -175,7 +136,8 @@ func (a *App) Routes() http.Handler {
 		fileServer := http.FileServer(staticDir)
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			// Check if the requested file exists in static dir
-			if _, err := staticDir.Open(r.URL.Path); err == nil {
+			if f, err := staticDir.Open(r.URL.Path); err == nil {
+				_ = f.Close()
 				a.logger.Debug("Serving static file", slog.String("path", r.URL.Path))
 				fileServer.ServeHTTP(w, r)
 				return

thaorell/kubeflow-notebooks

workspaces/backend/api/app.go
@@ -37,45 +37,6 @@ import (
 	_ "github.com/kubeflow/notebooks/workspaces/backend/openapi"
 )
 
-const (
-	Version    = "1.0.0"
-	PathPrefix = "/api/v1"
-
-	MediaTypeJson = "application/json"
-	MediaTypeYaml = "application/yaml"
-
-	NamespacePathParam    = "namespace"
-	ResourceNamePathParam = "name"
-
-	// healthcheck
-	HealthCheckPath = PathPrefix + "/healthcheck"
-
-	// user
-	UserPath = PathPrefix + "/user"
-
-	// workspaces
-	AllWorkspacesPath         = PathPrefix + "/workspaces"
-	WorkspacesByNamespacePath = AllWorkspacesPath + "/:" + NamespacePathParam
-	WorkspacesByNamePath      = AllWorkspacesPath + "/:" + NamespacePathParam + "/:" + ResourceNamePathParam
-	WorkspaceActionsPath      = WorkspacesByNamePath + "/actions"
-	PauseWorkspacePath        = WorkspaceActionsPath + "/pause"
-
-	// workspacekinds
-	AllWorkspaceKindsPath    = PathPrefix + "/workspacekinds"
-	WorkspaceKindsByNamePath = AllWorkspaceKindsPath + "/:" + ResourceNamePathParam
-
-	// namespaces
-	AllNamespacesPath = PathPrefix + "/namespaces"
-
-	// secrets
-	SecretsByNamespacePath = PathPrefix + "/secrets/:" + NamespacePathParam
-	SecretsByNamePath      = SecretsByNamespacePath + "/:" + ResourceNamePathParam
-
-	// swagger
-	SwaggerPath    = PathPrefix + "/swagger/*any"
-	SwaggerDocPath = PathPrefix + "/swagger/doc.json"
-)
-
 type App struct {
 	Config               *config.EnvConfig
 	logger               *slog.Logger
@@ -131,7 +92,7 @@ func (a *App) Routes() http.Handler {
 	router.GET(constants.HealthCheckPath, a.GetHealthcheckHandler)
 
 	// user
-	router.GET(UserPath, a.GetUserHandler)
+	router.GET(constants.UserPath, a.GetUserHandler)
 
 	// namespaces
 	router.GET(constants.AllNamespacesPath, a.GetNamespacesHandler)
@@ -181,13 +142,14 @@ func (a *App) Routes() http.Handler {
 
 	mux := http.NewServeMux()
 
-	mux.Handle(PathPrefix+"/", a.recoverPanic(a.enableCORS(handler)))
+	mux.Handle(constants.PathPrefix+"/", a.recoverPanic(a.enableCORS(handler)))
 
 	if a.Config.StaticAssetsDir != "" {
 		staticDir := http.Dir(a.Config.StaticAssetsDir)
 		fileServer := http.FileServer(staticDir)
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			if _, err := staticDir.Open(r.URL.Path); err == nil {
+			if f, err := staticDir.Open(r.URL.Path); err == nil {
+				_ = f.Close()
 				a.logger.Debug("Serving static file", slog.String("path", r.URL.Path))
 				fileServer.ServeHTTP(w, r)
 				return